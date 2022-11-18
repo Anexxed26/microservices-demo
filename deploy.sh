db0454f77469329e901e59a60df8201cb3fcca57
Walisson <walisson_05@outlook.com>
2022-11-18 00:10:24 +0000

    finalização do trabalho

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,13 @@
 
 ################################################WALISSON################################################
 
-minikube kubectl -- apply -f ./kubernetes-manifests/adservice.yaml
-
-minikube kubectl -- apply -f ./kubernetes-manifests/cartservice.yaml
-
-minikube kubectl -- apply -f ./kubernetes-manifests/checkoutservice.yaml
-
-minikube kubectl -- apply -f ./kubernetes-manifests/currencyservice.yaml
-
 minikube kubectl -- apply -f ./kubernetes-manifests/emailservice.yaml
 
 minikube kubectl -- apply -f ./kubernetes-manifests/frontend.yaml
+
+minikube kubectl -- apply -f ./kubernetes-manifests/recommendationservice.yaml
+
+minikube kubectl -- apply -f ./kubernetes-manifests/checkoutservice.yaml
 
 minikube kubectl -- apply -f ./kubernetes-manifests/loadgenerator.yaml
 
@@ -20,7 +16,11 @@ minikube kubectl -- apply -f ./kubernetes-manifests/paymentservice.yaml
 
 minikube kubectl -- apply -f ./kubernetes-manifests/productcatalogservice.yaml
 
-minikube kubectl -- apply -f ./kubernetes-manifests/recommendationservice.yaml
+minikube kubectl -- apply -f ./kubernetes-manifests/cartservice.yaml
+
+minikube kubectl -- apply -f ./kubernetes-manifests/currencyservice.yaml
+
+minikube kubectl -- apply -f ./kubernetes-manifests/adservice.yaml
 
 minikube kubectl -- apply -f ./kubernetes-manifests/redis.yaml
 
